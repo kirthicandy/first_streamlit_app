602ab93d824db1f0308fbcc67b62488e5bed0887
Kiruthika P <106480699+kirthicandy@users.noreply.github.com>
2023-07-27 17:06:59 +0530

    Update streamlit_app.py
    secord text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,7 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load list contain:")
 streamlit.dataframe(my_data_row)
+
+#second text entry box
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding jackfruit ', add_my_fruit)
