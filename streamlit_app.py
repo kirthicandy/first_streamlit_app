026a3528b54a54e3665898e7747e9907c5f7241c
Kiruthika P <106480699+kirthicandy@users.noreply.github.com>
2023-07-27 12:41:24 +0530

    Update streamlit_app.py
    etye

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 streamlit.title(' My Parents New Healthy Diner');
 
 
-streamlit.header(' Breakfast Menu');
+streamlit.header(' Breakfast ');
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal');
 streamlit.text('🥗 Kale,Spinach & Rocket Smoothie');
 streamlit.text('🐔 Hard-Boiled Free-Range Egg');
